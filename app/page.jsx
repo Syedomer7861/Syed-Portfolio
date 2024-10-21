d49a27eb98c6358401b94964d0228ef360b5d71b
Syed Omer Farrukh <63002717+Syedomer7861@users.noreply.github.com>
2024-10-21 17:01:47 +0400

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -24,7 +24,7 @@ const Home = () => {
             </p>
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button href="https://www.instagram.com/syed_omer7861/"
+              <Button path="https://www.instagram.com/syed_omer7861/"
                 variant="outline"
                 size="lg"
                 className="uppercase flex items-center gap-2"
